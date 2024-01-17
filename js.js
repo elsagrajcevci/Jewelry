854b61ea7574487fd93536596d0f6e604d666347
elsagrajcevci <156684183+elsagrajcevci@users.noreply.github.com>
2024-01-17 13:41:57 +0100

    Ndryshimi 1

--- js.js
+++ js.js
@@ -5,7 +5,7 @@ function shtoNeShporte(emriProduktit, cmimiProduktit) {
     shporta.push({ emri: emriProduktit, cmimi: cmimiProduktit });
     totali += cmimiProduktit;
 
-    // Rifresko shfaqjen e shportës
+    
     shfaqShporten();
 }
 
@@ -13,10 +13,10 @@ function shfaqShporten() {
     let shportaLista = document.getElementById("shporta-lista");
     let totaliSpan = document.getElementById("totali");
 
-    // Fshij të dhënat ekzistuese në shportë
+    
     shportaLista.innerHTML = "";
 
-    // Shto produkte të reja në shportë
+
     shporta.forEach(function (produkt) {
         let produktItem = document.createElement("li");
         produktItem.textContent = produkt.emri + " - $" + produkt.cmimi;
@@ -27,8 +27,7 @@ function shfaqShporten() {
 }
 
 function perfundoPorosine() {
-    // Këtu mund të shtoni logjikën për përfundimin e porosisë
     alert("Porosia është përfunduar me sukses!");
     
-    // Pastaj mund të bëni gjëra tjera, si të pastrohen produktet në shportë, të rifreskohet faqja, etj.
+
 }
